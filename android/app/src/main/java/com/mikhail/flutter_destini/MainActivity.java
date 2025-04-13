baed38b9239eb0b6f85f2a7c505ad640603c8d14
Misha (MacBook) <macbook@MacBook-Air.local>
2025-04-13 01:39:42 +0100

    Initial commit: project is created

---
+++ android/app/src/main/java/com/mikhail/flutter_destini/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mikhail.flutter_destini;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Destini());
+}
+
+class Destini extends StatelessWidget {
+  const Destini({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
