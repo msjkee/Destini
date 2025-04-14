4bcf54d94d48ad79e1d1a9acbbcd31f5b74c90e7
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-04-14 22:05:07 +0100

    Add fully functional Destini story app with storyline navigation and restart logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_destini/StoryBrain.dart';
 
 void main() {
   runApp(const Destini());
@@ -9,10 +10,103 @@ class Destini extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        //TODO: DESTINI GAME (tomorrow)
-      )
+    return MaterialApp(theme: ThemeData.dark(), home: StoryPage());
+  }
+}
+
+StoryBrain storyBrain = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget> [
+              Expanded(
+                flex: 6,
+                child: Center(
+                  child: Text(
+                      storyBrain.getStory(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                    )
+                  )
+                )
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero
+                    )
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0
+                    )
+                  )
+                )
+              ),
+              SizedBox(
+                height: 25.0
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero
+                      )
+                    ),
+                      onPressed: () {
+                        setState(() {
+                          storyBrain.nextStory(2);
+                        });
+                      },
+                      child: Text(
+                          storyBrain.getChoice2(),
+                        style: TextStyle(
+                          fontSize: 20.0
+                        )
+                      )),
+                )
+              )
+            ],
+          )
+        ),
+      ),
     );
   }
 }
